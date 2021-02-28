dbcfe1138070775f849db494ae7d05862fe4460c
mibo <mibo@mirb.de>
2021-02-28 08:47:41 +0100

    Fixed tests

--- src/test/java/com/github/mibo/pg/quarkus/boundary/GreetingResourceTest.java
+++ src/test/java/com/github/mibo/pg/quarkus/boundary/GreetingResourceTest.java
@@ -12,10 +12,26 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello-resteasy")
+          .when().get("/hello/resteasy")
           .then()
              .statusCode(200)
              .body(is("Hello RESTEasy"));
     }
 
+    @Test
+    public void testGreetWithNameEndpoint() {
+        given()
+          .when().get("/hello/greet/test-name")
+          .then()
+             .statusCode(200)
+             .body(is("Hello test-name"));
+    }
+
+    @Test
+    public void testNotFoundEndpoint() {
+        given()
+          .when().get("/hello/not-found")
+          .then()
+             .statusCode(404);
+    }
 }
